952e3412b2dce3a8695ba457db017f51d50a0082
anara01 <anaraerkebaeva2@gmail.com>
2022-05-17 15:29:58 +0300

    ValidationError for forms

--- news/forms.py
+++ news/forms.py
@@ -1,8 +1,7 @@
-from dataclasses import field
-from pyexpat import model
-from tkinter import Widget
 from django import forms
 from .models import News
+import re
+from django.core.exceptions import ValidationError
 
 #from .models import Category
 
@@ -30,3 +29,9 @@ class NewsForm(forms.ModelForm):
             'content': forms.Textarea(attrs={"class": "form-control", "rows": 5}),
             'category': forms.Select(attrs={"class": "form-control"})
         }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
