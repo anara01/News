387d2f9c4ae3ada44bfabd0a34c598ef30c6a4b9
anara01 <anaraerkebaeva2@gmail.com>
2022-05-22 17:10:38 +0300

    AuthenticationForm, login, logout

--- news/forms.py
+++ news/forms.py
@@ -4,14 +4,14 @@ from tkinter import Widget
 from django import forms
 from .models import News
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
 class UserRefisterForm(UserCreationForm):
     username = forms.CharField(label='Имя пользователя',
-                               help_text='Максимум 150 символов', widget=forms.TextInput(
-                                   attrs={"class": "form-control"}))
+                               help_text='Максимум 150 символов',
+                               widget=forms.TextInput(attrs={"class": "form-control"}))
     password1 = forms.CharField(label='Пароль',
                                 widget=forms.PasswordInput(attrs={"class": "form-control"}))
     password2 = forms.CharField(label='Подтверждение пароля',
@@ -22,6 +22,13 @@ class UserRefisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя',
+                               widget=forms.TextInput(attrs={"class": "form-control"}))
+    password = forms.CharField(label='Пароль',
+                               widget=forms.PasswordInput(attrs={"class": "form-control"}))
 
 
 # from .models import Category
